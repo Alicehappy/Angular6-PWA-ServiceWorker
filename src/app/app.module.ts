8c60cb8f412b38f3bc1922e75e81e58125359f38
Alice Zou <alice@datafax.com>
2018-06-19 11:00:05 -0400

    Angular Service Worker - Step by step turning this angular 6 application in to PWA app. First, intall service worker; then, using production mode(service worker allows app be used offline mode); Thirdly, set an available observable; Fourthly, set one-click install with app manifest. Guide link: https://blog.angular-university.io/angular-service-worker/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { SwUpdate } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
